52b30d361a63caefa191b8129dc12fead5aa919c
Sonugc <sonugc72@gmail.com>
2023-07-03 14:54:30 +0545

    list print using continue

--- exercises/python_basics.py
+++ exercises/python_basics.py
@@ -11,8 +11,15 @@
 #         break
 #     print(i)
 
+# for i in range(5):
+#     if i==3 :
+#         continue
+#     print(i)
+
+
 for i in range(5):
     if i==3 :
         continue
-    print(i)
+    else :
+        print(i)
 
