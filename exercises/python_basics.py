3b5fd66baedac3a4df44639ad96488c6858e8daa
Sonugc <sonugc72@gmail.com>
2023-07-03 14:40:59 +0545

    list print

--- exercises/python_basics.py
+++ exercises/python_basics.py
@@ -1,2 +1,7 @@
-print("hello world")
-print("New hello world")
+#printing the numbers from 1 to 5
+for i in [1,2,3,4,5]:
+   print(i)
+
+#printing the number from 0 to 5   
+#for i in range(5):
+#    print(i)
