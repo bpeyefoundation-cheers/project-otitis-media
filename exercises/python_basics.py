31201860e1f73ec61ba01131ddb9709da1503923
ayushma77 <pandeyayushma77@gmail.com>
2023-07-03 15:02:34 +0545

    printing triangle

--- exercises/python_basics.py
+++ exercises/python_basics.py
@@ -18,6 +18,13 @@ for i in range(5):
           continue
      else:
           print(i)
+
+for i in range(1,6):
+     for j in range(1,i+1):
+          print(j,end=' ')
+     print()
+     
+     
       
 
 
