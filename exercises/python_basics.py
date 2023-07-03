91c28b405d58037af99e43ac9ee12a9350b9818a
ayushma77 <pandeyayushma77@gmail.com>
2023-07-03 15:07:29 +0545

    printing variable

--- exercises/python_basics.py
+++ exercises/python_basics.py
@@ -23,6 +23,15 @@ for i in range(1,6):
      for j in range(1,i+1):
           print(j,end=' ')
      print()
+
+a=[1,4,9,16,25,36,49,64,81,100]
+print(a[0])
+print(a[-1])
+print(a[-2])
+
+b=a[:5]
+print(b)
+
      
      
       
