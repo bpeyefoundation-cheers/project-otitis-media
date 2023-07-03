d3d432b16c6edf6f8ed924adf57f7a1a0d87f117
Sonugc <sonugc72@gmail.com>
2023-07-03 14:42:42 +0545

    list print using range

--- exercises/python_basics.py
+++ exercises/python_basics.py
@@ -1,7 +1,7 @@
 #printing the numbers from 1 to 5
-for i in [1,2,3,4,5]:
-   print(i)
+#for i in [1,2,3,4,5]:
+#   print(i)
 
 #printing the number from 0 to 5   
-#for i in range(5):
-#    print(i)
+for i in range(5):
+    print(i)
