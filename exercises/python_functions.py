35ee7007669fe64527a3b2e7d9032dea2489bcf5
ayushma77 <pandeyayushma77@gmail.com>
2023-07-04 15:26:04 +0545

    exercises

---
+++ exercises/python_functions.py
@@ -0,0 +1,43 @@
+def add(a,b):
+    c=a+b
+    return c
+s=add(1,2)
+print(s)
+
+def create_lst(lst):
+    emp_lst=[]
+    for e in lst:
+        
+        emp_lst.append(e*2)
+
+
+    return emp_lst
+double_lst=create_lst([1,2,3,4,5])
+print(double_lst)
+
+a=[e*2 for e in [2,4,6,8]] 
+print(a)
+
+def add_lst(l1,l2):
+    output_lst=[]
+    for i,j in zip(l1,l2):
+        sum=i+j
+        output_lst.append(sum)
+
+    return output_lst
+sum_list=add_lst([1,2,3,4],[2,4,6,7])
+print(sum_list)
+
+#printing _number
+def print_pattern(num):
+    for i in range(1,num+1):
+        for j in range(1,i+1):
+            print(j,end=' ')
+        print()
+
+print(print_pattern(6))
+
+
+
+
+    
