ea283efb258d9db7c0e996f7d5baa4aeed9cb133
Sonugc <sonugc72@gmail.com>
2023-07-04 14:57:19 +0545

    double list commited

--- exercises/python_functions.py
+++ exercises/python_functions.py
@@ -1,7 +1,17 @@
 # write a function called 'add' that takes two numbers as arguments
 
-def add(x, y):
-    sum= x+y
-    return sum
-sum = add(5,6)
-print(sum)
+# def add(x, y):
+#     sum= x+y
+#     return sum
+# sum = add(5,6)
+# print(sum)
+
+#list
+
+first_list=[1,2,3,4,5]
+def create_list(first_list):
+    new_list=[i*2 for i in first_list]
+    return new_list
+
+double_valued_list = create_list(first_list)  
+print(double_valued_list)  
