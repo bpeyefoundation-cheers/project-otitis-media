68072e310d66cc9f3960bac3baf119be06fe00a5
Sonugc <sonugc72@gmail.com>
2023-07-04 14:40:56 +0545

    add function commited

--- exercises/python_basics.py
+++ exercises/python_basics.py
@@ -42,10 +42,51 @@
 
 
 #print list using loop
-a=[]
-for i in  range(1, 11):
-    a.append(pow(i,2))
-print(a)
+# a = [i**2 for i in range(10)]
+# print(a[::2])
+
+# a=[]
+# for i in  range(1, 11):
+#     a.append(pow(i,2))
+# print(a)
+# print(a[0])
+# print(a[1])
+# print(a[9])
+# print(a[-2])
+# print(len(a))
+
+## store the first 5 elements of a into a new variable and print the new variable 
+# b=[]
+# b.append(a[0:5])
+# print(b)
+
+
+# create a dictionary with the keys
+
+bp_eye_daily_patient_enrollment = {
+    'ent': 100,
+    'dental' : 50,
+    'eye' : 200
+}
+
+
+# increase all the enrolled patient  in patient enrollment dict by 50
+for patient in bp_eye_daily_patient_enrollment:
+    bp_eye_daily_patient_enrollment[patient]+= 50
+print(bp_eye_daily_patient_enrollment)
+
+
+#alternative
+for k, v in bp_eye_daily_patient_enrollment.items():
+    bp_eye_daily_patient_enrollment[k] = v+50
+print(bp_eye_daily_patient_enrollment)
+
+#using keys
+for k in bp_eye_daily_patient_enrollment.keys():
+    bp_eye_daily_patient_enrollment[k] = bp_eye_daily_patient_enrollment[k]+50
+print(bp_eye_daily_patient_enrollment)
+
+
 
 
 
---
+++ exercises/python_functions.py
@@ -0,0 +1,7 @@
+# write a function called 'add' that takes two numbers as arguments
+
+def add(x, y):
+    sum= x+y
+    return sum
+sum = add(5,6)
+print(sum)
