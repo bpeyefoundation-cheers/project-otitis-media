bac4f6089172ac11d85a0ef391e426d3f548377b
Mansish01 <gyawalimanish12345@gmail.com>
2023-07-11 15:56:27 +0545

    data to the csv file

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,5 +1,6 @@
 import unittest
-from utils.io import list_files
+from utils.io import list_files, get_image_label_pairs
+
 
 class TestIO(unittest.TestCase):
     def test_list_files(self):
@@ -9,3 +10,8 @@ class TestIO(unittest.TestCase):
         
        # self.assertIsNotNone(list_files('data\middle-ear-dataset/am', ''))
         
+    def test_get_image_label_pairs(self):
+        self.assertIsNotNone(get_image_label_pairs('data\middle-ear-dataset/aom', 'aom'))
+        self.assertEqual(len(get_image_label_pairs('data\middle-ear-dataset/aom', 'aom')[0]), 119)
+        
+    
--- utils/io.py
+++ utils/io.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 def list_files(dir:str, file_extension:str) ->list:
     '''given a dorectory, list all files with given extension'''
@@ -6,8 +7,51 @@ def list_files(dir:str, file_extension:str) ->list:
     if not os.path.isdir(dir):
         return None
     
+    
     files= os.listdir(dir)
     return files
+
+def get_image_label_pairs(dir: str, label:str) ->tuple:
+    
+    filenames = list_files(dir, '')
+    labels = [label] * len(filenames)
+    return filenames, labels 
+
+def save_as_csv(img_path: str, label:str, outfile):
+    '''save image path and save as csv fille'''
+    with open(outfile , 'w') as f:
+        writer= csv.writer(f)
+        writer.writerow(['file', 'label'])
+        for image_path , label in zip(img_path , label):
+            writer.writerow([image_path, label])
+
+
+
+# if __name__ == '__main__':
+#     images , labels= get_image_label_pairs('data\middle-ear-dataset/aom', 'aom')
+#     images_1 , labels_1 = get_image_label_pairs('data\middle-ear-dataset/csom', 'csom')
+    
+#     save_as_csv(images, labels, 'data/AOM.csv')
+#     save_as_csv(images_1, labels_1, 'data/CSOM.csv')
+
+folder= ['aom', 'csom', 'myringosclerosis', 'Normal']
+
+if __name__ == '__main__':
+    
+    x=[]
+    y=[]
+    for i in folder:
+        path= f'data/middle-ear-dataset/{i}'
+        images, label=get_image_label_pairs(path, f'{i}')
+        x.extend(images)
+        y.extend(label)
+        
+        save_as_csv(x, y, 'data/data.csv')    
+    
+
+
+
+
         
     
 
