95d6817f09ebc7a828d2ca2d7e95c685a7733fb3
ayushma77 <pandeyayushma77@gmail.com>
2023-07-18 16:21:20 +0545

    converting label to index and vive versa in preprocessing.py

--- scripts/train_test_scripts.py
+++ scripts/train_test_scripts.py
@@ -3,12 +3,30 @@ from os.path import join
 from utils.io import get_image_label_pairs
 from sklearn.model_selection import train_test_split
 from utils.io import save_as_csv
+import argparse
+from argparse import ArgumentParser,BooleanOptionalAction
 
-DATA_DIR = "data\middle-ear-dataset"
+parser = argparse.ArgumentParser(description='test_train_scripting')
+parser.add_argument('-d','--data-dir',type=str, help='data directory',default="data\middle-ear-dataset")
+parser.add_argument('-t','--test-size',type=float,help='test size',default=0.2)
+parser.add_argument('-r','--random-state',type=int,default=42)
+parser.add_argument('-s','--shuffle',type=str,default=True)
+
+parser.add_argument('--do-shuffle',action=BooleanOptionalAction)
+
+
+args=parser.parse_args()
+print(args.data_dir)
+print(args.test_size)
+print(args.random_state)
+print(args.shuffle)
+
+
+DATA_DIR = args.data_dir
 
 data_folders = os.listdir(DATA_DIR)
 
-print(data_folders)
+#print(data_folders)
 
 X = []
 y = []
@@ -20,16 +38,14 @@ for folders in data_folders:
     X.extend(file_names)
     y.extend(labels)
 
-print(len(X), len(y))
+# print(len(X), len(y))
 
 # splitting dataset into train and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print('X train size:',len(X_test))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=args.test_size, random_state=args.random_state,shuffle=args.shuffle,stratify=None)
+print('X test size:',len(X_test))
 print("Y test size:",len(y_test))
 
 #save tthe train and test sets as csv
 save_as_csv(X_train,y_train,'data/train.csv')
 save_as_csv(X_test,y_test,'data/test.csv')
-
-
 
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -73,22 +73,33 @@ def display_grid(DATA_DIR,image_files,labels,n_rows,n_cols,title,figsize=(10,10)
 
             
             
-  
+ 
+label_to_idx_map = {'aom': 0, 'csom': 1, 'myringosclerosis': 2,'Normal':3}
+rev_label_to_idx_map={index:label for label,index in label_to_idx_map.items() }
 
+def label_to_idx(label:str):
+        """convert label name to index
+	for example: if my dataset consists of three labels (aom, csom, myringosclerosis,normal)
+	this function should return 0 for aom, 1 for csom, 2 for myringosclerosis,normal
+	"""
+        if label not in label_to_idx_map:
+             raise KeyError(f"label not define . Defined label are:{label_to_idx_map.keys()}")
+        return label_to_idx_map[label]
 
-# def label_to_idx(label:str):
-# 	"""convert label name to index
-# 	for example: if my dataset consists of three labels (setosa, virginica, versicolor)
-# 	this function should return 0 for setosa, 1 for virginica, 2 for versicolor
-# 	"""
-# 	label_map = {'aom': 0, 'csom': 1, 'myringosclerosis': 2,'Normal':3}
-# 	return label_map.get(label)
+   
 
+def idx_to_label(idx:int):
+        """ similiar as label_to_idx but opposite I.e. take the index and return the string label """
+        try:
+            return rev_label_to_idx_map[idx]
+        except KeyError:
+            raise KeyError(f"Label not found. Try one of these: {rev_label_to_idx_map.keys()}")
 
-# def idx_to_label(idx:int):
-# 	""" similiar as label_to_idx but opposite I.e. take the index and return the string label """
-# 	label_map = {0: 'aom', 1: 'csom', 2: 'myringosclerosis',3: 'Normal'}
-# 	return label_map.get(idx)
+# def func(default=None):
+#     try:
+#         return label_to_idx_map[label]
+#     except KeyError:
+#          return default
 
 if __name__ == "__main__":
     # IMAGE_PATH=r'C:\Users\Dell\Desktop\projects\project-otitis-media\data\middle-ear-dataset\csom\o13.jpg'
@@ -99,13 +110,23 @@ if __name__ == "__main__":
     # plt.yticks([])
     # set title
 
-    DATA_DIR = "data/middle-ear-dataset/csom"
-    image_files = os.listdir(DATA_DIR)
-    n_rows=4
-    n_cols=3
-    LABEL='csom'
-    file_path,labels=get_image_label_pairs(DATA_DIR,LABEL)
-    # Create a 4x3 grid of subplots
+    #check for label to index
+    label='aom'
+    label_idx=label_to_idx(label)
+    print(label_idx)
+
+    # check for index to label
+    index=0
+    idx_label=idx_to_label(index)
+    print(idx_label)
+
+    # DATA_DIR = "data/middle-ear-dataset/csom"
+    # image_files = os.listdir(DATA_DIR)
+    # n_rows=4
+    # n_cols=3
+    # LABEL='csom'
+    # file_path,labels=get_image_label_pairs(DATA_DIR,LABEL)
+    # # Create a 4x3 grid of subplots
     # fig, axes = plt.subplots(n_rows, n_cols, figsize=(5, 5))
     # idx=0
     # counter = 0
@@ -127,5 +148,5 @@ if __name__ == "__main__":
 
             # counter += 1
 			
-display_grid(DATA_DIR,file_path,labels,n_rows,n_cols,'middle_ear')
+# display_grid(DATA_DIR,file_path,labels,n_rows,n_cols,'middle_ear')
     
