0f762244185f7728ed82558eb2e1a628b670dcf9
Mansish01 <gyawalimanish12345@gmail.com>
2023-08-01 16:00:18 +0545

    gates and learning rate

--- perceptron.py
+++ perceptron.py
@@ -1,42 +1,60 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def calculate_weighted_sum( input , weights, intercept):
-    weighted_sum = np.dot(input , weights) + intercept 
-    return weighted_sum
-
-def perceptron_output(input, weights, intercept):
-     weight = calculate_weighted_sum(input , weights, intercept)
-     value= np.where( weight < 0 , -1 , 1)
-     return value
-
-def perceptron_learning( inputs , weights , intercept , target):
-    output = perceptron_output(inputs , weights , intercept)
-    error = np.where( target != output ,  1 , 0 ) * target
+class Perceptron:
+    def __init__(self, input) :
+        self.weight=np.array([0,0])
+        self.intercept=0
+        
+    def calc_weight_sum(self,input):
     
-    #n_weight  = weights * error
-    #new_weight = np.dot(inputs , n_weight)
-    new_weight = np.dot( inputs , error ) + weights 
-    updated_intercept = intercept + error
-    return new_weight , updated_intercept
-    
-    
-     
+        weighted_sum=np.dot(input,self.weight)+self.intercept
+        return weighted_sum
+    def predict(self,input):
+        weighted_sum=self.calc_weight_sum(input)
+        percep_output=np.where(weighted_sum<0,-1,1)
+        return percep_output
+    def fit(self,input,target,epochs):
+        for _ in range(epochs):
+            for x, y in zip(input, target):
+                output = self.predict(x)
+                error = np.where(y != output, 1, 0) * y
+                self.weight += x * error
+                self.intercept += y
+                
+                
+    def plot_decision_boundary(self, inputs:np.ndarray, targets:np.ndarray) -> None:
+        # Generate data points for the decision boundary line
+        x_vals = np.linspace(np.min(inputs[:, 0]), np.max(inputs[:, 0]), 100)
+        y_vals = -(self.weight[0] / self.weight[1]) * x_vals - (self.intercept / self.weight[1])
 
+        # Plot the data points
+        classes = np.unique(targets)
+        
+        assert len(classes) == 2, "Only binary classification is supported"
+
+        plt.scatter(inputs[targets == classes[0]][:, 0], inputs[targets == classes[0]][:, 1], c='b', label=f'Class {classes[0]}')
+        plt.scatter(inputs[targets == classes[1]][:, 0], inputs[targets == classes[1]][:, 1], c='r', label=f'Class {classes[1]}')
+
+        # Plot the decision boundary line
+        plt.plot(x_vals, y_vals, 'g', label='Decision Boundary')
+
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.legend()
+        plt.title('Decision Boundary Plot')
+        plt.grid(True)
+        plt.show()
 if __name__ == "__main__":
-    input=np.array([[1,2],[2,2],[4,5],[6,6]])
-    target= np.array([1, 1, -1, -1])
-    epoch = 100
-     
-    weights = np.array([0, 0])
-    '''bias and intercept are same '''
+        input_data = np.array([[1, 2], [3, 4], [5, 6], [4, 5]])
+        target = np.array([1, 1, -1, -1])
 
-    intercept= 0  
-    
-    for loop in range(epoch):
-        for inp, tar in zip(input , target):
-            weights, intercept= perceptron_learning(inp, weights, intercept, tar)
-            
-     #print(final_weight , final_intercept)
-    inp= input[0]
-    output = perceptron_output(inp , weights, intercept)
-    print(output)
+        perceptron = Perceptron(input_data)
+
+        epochs = 500
+        perceptron.fit(input_data, target, epochs)
+
+        #print("Updated weight:", perceptron.weight)
+        #print("Updated intercept:", perceptron.intercept)
+        
+        perceptron.plot_decision_boundary(input_data, target)
