9e2dc9d557886be2e0264f756d3291737bc0ea1d
ayushma77 <pandeyayushma77@gmail.com>
2023-08-02 12:20:01 +0545

    perceptron updated

--- perceptron.py
+++ perceptron.py
@@ -1,41 +1,5 @@
 import numpy as np
-# def calc_weight_sum(input:np.array,weight:np.array,intercept:float):
-
-#     weighted_sum=np.dot(input,weight)+intercept
-#     return weighted_sum
-
-# def perceptron_output(input,weight,intercept):
-#     weighted_sum=calc_weight_sum(input,weight,intercept)
-#     percep_output=np.where(weighted_sum<0,-1,1)
-#     return percep_output
-
-
-
-# def perceptron_learning(input,weight,intercept,target):
-#     output=perceptron_output(input,weight,intercept)
-#     error=np.where(target!=output,1,0)*target
-#     updated_weight=np.dot(input,error)+weight
-#     updated_intercept=intercept+target
-#     # print("Input",input,"Out",output,"Error",error,"target",target)
-#     return updated_weight,updated_intercept 
-
-
-# if __name__=="__main__":
-#     input=np.array([[1,2],[3,4],[5,6],[4,5]])
-#     weight=np.array([0,0])
-#     intercept=0
-#     target=np.array([1,1,-1,-1])
-
-#     epochs=10
-#     for e in range(epochs):
-#         for x,y in zip(input,target):
-       
-#             weight, intercept =perceptron_learning(x,weight,intercept,y)
-#         print("updated_result:", weight  ,intercept)
-
-# target_value=perceptron_output(input[0],weight,intercept)
-# print(target_value)
-
+import  matplotlib.pyplot as plt
 #using class
 class Perceptron:
     def __init__(self,input) :
@@ -46,17 +10,41 @@ class Perceptron:
     
         weighted_sum=np.dot(input,self.weight)+self.intercept
         return weighted_sum
-    def perceptron_output(self,input):
+    def predict(self,input):
         weighted_sum=self.calc_weight_sum(input)
         percep_output=np.where(weighted_sum<0,-1,1)
         return percep_output
-    def perceptron_learning(self,input,target,epochs):
+    def fit(self,input,target,epochs):
         for _ in range(epochs):
             for x, y in zip(input, target):
-                output = self.perceptron_output(x)
+                output = self.predict(x)
                 error = np.where(y != output, 1, 0) * y
                 self.weight += x * error
                 self.intercept += y
+
+    def plot_decision_boundary(self, inputs:np.ndarray, targets:np.ndarray) -> None:
+    # Generate data points for the decision boundary line
+        x_vals = np.linspace(np.min(inputs[:, 0]), np.max(inputs[:, 0]), 100)
+        y_vals = -(self.weight[0] / self.weight[1]) * x_vals - (self.intercept / self.weight[1])
+
+        # Plot the data points
+        classes = np.unique(targets)
+        
+        assert len(classes) == 2, "Only binary classification is supported"
+
+        plt.scatter(inputs[targets == classes[0]][:, 0], inputs[targets == classes[0]][:, 1], c='b', label=f'Class {classes[0]}')
+        plt.scatter(inputs[targets == classes[1]][:, 0], inputs[targets == classes[1]][:, 1], c='r', label=f'Class {classes[1]}')
+
+        # Plot the decision boundary line
+        plt.plot(x_vals, y_vals, 'g', label='Decision Boundary')
+
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.legend()
+        plt.title('Decision Boundary Plot')
+        plt.grid(True)
+        plt.show()
+         
 if __name__ == "__main__":
         input_data = np.array([[1, 2], [3, 4], [5, 6], [4, 5]])
         target = np.array([1, 1, -1, -1])
@@ -64,7 +52,8 @@ if __name__ == "__main__":
         perceptron = Perceptron(input_data)
 
         epochs = 10
-        perceptron.perceptron_learning(input_data, target, epochs)
+        perceptron.fit(input_data, target, epochs)
+        perceptron.plot_decision_boundary(input_data, target)
 
         print("Updated weight:", perceptron.weight)
         print("Updated intercept:", perceptron.intercept)
