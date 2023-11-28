28f7c76f05b6605b2da293f7ab8d9c36cac8113a
ayushma77 <pandeyayushma77@gmail.com>
2023-11-28 16:02:34 +0545

    accuracy and loss visualization

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from datasets.image_datasets import ImageDataset
 from torch.utils.data import DataLoader
 from models.CustomNN import Model,OtitisMediaClassifier
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
   
         
 #training
 
 if __name__=="__main__":
+    SEED=42
+    torch.manual_seed(SEED)
     BATCH_SIZE=16
     #prepare dataset
     train_csv_path=r"data\train.csv"
@@ -41,17 +45,26 @@ if __name__=="__main__":
 
 
     #training
-    LR=0.01
-    EPOCHS=50
+    LR=0.001
+    EPOCHS=10
     criterion=nn.NLLLoss()
     optimizer=torch.optim.Adam(model.parameters(),lr=LR)
+    epochwise_train_loss=[]
+    epochwise_val_loss=[]
+    epochwise_val_accuracy=[]
+    epochwise_train_accuracy=[]
+
     for epoch in range(EPOCHS):
         train_running_loss=0
         val_running_loss=0
+        train_running_accuracy=0
+        val_running_accuracy=0
+
+        
         #traing
         model.train()
 
-
+       
         for images,labels in train_data_loader:
             
             optimizer.zero_grad()
@@ -62,26 +75,89 @@ if __name__=="__main__":
             train_running_loss+=loss.item()
             loss.backward()
             optimizer.step()
+            # # calculate train_accuracy
+            # preds=torch.argmax(model_out,dim=1)
+            # accuracy=(preds==labels).float().mean()
+            # val_running_accuracy+=accuracy.item()
+            
             
           #validation
         model.eval()
-        for images,labels in val_data_loader:
+        for images,labels in train_data_loader:
           
           # optimizer.zero_grad()
+          model_out=model(images)
+          model_out=F.log_softmax(model_out,dim=1)
+          # calculate val_accuracy
+          preds=torch.argmax(model_out,dim=1)
+          accuracy=(preds==labels).float().mean()
+          train_running_accuracy+=accuracy.item()
+
+        for images,labels in val_data_loader:
+          
+          
           model_out=model(images)
           model_out=F.log_softmax(model_out,dim=1)
 
           loss=criterion(model_out,labels)
           val_running_loss+=loss.item()
           loss.backward()
-          optimizer.step()
-          # calculate accuracy
-          values,indices=torch.max(model_out,dim=1)
-          indices==labels
+          
+          # calculate val_accuracy
+          preds=torch.argmax(model_out,dim=1)
+          accuracy=(preds==labels).float().mean()
+          val_running_accuracy+=accuracy.item()
+          
         
           
         avg_train_loss=train_running_loss/len(train_data_loader)
         avg_val_loss=val_running_loss/len(val_data_loader)
 
-        print(f"Epoch {epoch} Train_Loss:{avg_train_loss:.3f} \t Val Loss:{avg_val_loss:.3f}")
-     
+        avg_val_running_accuracy=val_running_accuracy/len(val_data_loader)
+        avg_train_running_accuracy=train_running_accuracy/len(train_data_loader)
+
+
+        epochwise_train_loss.append(avg_train_loss)
+        epochwise_val_loss.append(avg_val_loss)
+
+        epochwise_val_accuracy.append(avg_val_running_accuracy)
+        epochwise_train_accuracy.append(avg_train_running_accuracy)
+
+        print(f"Epoch {epoch} Train_Loss:{avg_train_loss:.3f} \t Val Loss:{avg_val_loss:.3f}  \t Tain Accuracy:{avg_train_running_accuracy:.2f}  \t Val Accuracy:{avg_val_running_accuracy:.2f}")
+    #save the model
+    torch.save(model.state_dict(),"artifacts/first_model.pth")
+    torch.save(model,"artifacts/first_model_1.pt")
+
+
+
+
+    fig,(ax1,ax2)=plt.subplots(1,2,figsize=(16,9))
+    x_axis_values=np.arange(0,EPOCHS,1)
+    ax1.plot(epochwise_train_accuracy,label='train accuracy')
+    ax1.plot(epochwise_val_accuracy,label='val accuracy')
+    ax1.set_title("train vs val accuracy")
+    ax1.legend()
+  
+
+    ax2.plot(epochwise_val_loss,label='val loss')
+    ax2.plot(epochwise_train_loss,label='train loss')
+    ax2.set_title("train vs val loss")
+    ax2.legend()
+   
+    plt.title("Loss vs accuracy")
+    plt.show()
+
+
+    
+    
+
+
+
+
+    
+   
+
+
+
+      
+        
