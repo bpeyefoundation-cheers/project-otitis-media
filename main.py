50064de8c0de175af1402a42f2933cd84774a170
ayushma77 <pandeyayushma77@gmail.com>
2023-11-29 16:10:31 +0545

    best_checkpoint_name

--- main.py
+++ main.py
@@ -9,14 +9,27 @@ from models.CustomNN import Model,OtitisMediaClassifier
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
-  
+import os
+from uuid import uuid4
+from datetime import datetime  
         
 #training
 
 if __name__=="__main__":
+    best_val_accurcy=0
     SEED=42
     torch.manual_seed(SEED)
     BATCH_SIZE=16
+    #create folder
+    #get the current date and time
+    dt=datetime.now()
+    #format the datime with custom separators
+    f_dt=dt.strftime("%Y-%m-%d-%H-%M-%S")
+    folder_name=f"run-{f_dt}"
+
+
+    os.mkdir(f"artifacts/{folder_name}")
+    print(f"folder name:{folder_name}")
     #prepare dataset
     train_csv_path=r"data\train.csv"
     val_csv_path=r"data\test.csv"
@@ -37,6 +50,8 @@ if __name__=="__main__":
 
     #prepare model
     model=OtitisMediaClassifier(img_size=256,num_channels=3,num_labels=4)
+    #print model name
+    
     #print(model(x))
 #     model=Model()
 #     pred=model(x)
@@ -108,6 +123,8 @@ if __name__=="__main__":
           accuracy=(preds==labels).float().mean()
           val_running_accuracy+=accuracy.item()
           
+
+          
         
           
         avg_train_loss=train_running_loss/len(train_data_loader)
@@ -116,18 +133,33 @@ if __name__=="__main__":
         avg_val_running_accuracy=val_running_accuracy/len(val_data_loader)
         avg_train_running_accuracy=train_running_accuracy/len(train_data_loader)
 
+        if avg_val_running_accuracy>best_val_accurcy:
+           best_val_accurcy=avg_val_running_accuracy
+           torch.save(model.state_dict(),f"artifacts/{folder_name}/best_model.pth")
+
 
         epochwise_train_loss.append(avg_train_loss)
         epochwise_val_loss.append(avg_val_loss)
 
         epochwise_val_accuracy.append(avg_val_running_accuracy)
         epochwise_train_accuracy.append(avg_train_running_accuracy)
-
+        
+        
         print(f"Epoch {epoch} Train_Loss:{avg_train_loss:.3f} \t Val Loss:{avg_val_loss:.3f}  \t Tain Accuracy:{avg_train_running_accuracy:.2f}  \t Val Accuracy:{avg_val_running_accuracy:.2f}")
-    #save the model
-    torch.save(model.state_dict(),"artifacts/first_model.pth")
-    torch.save(model,"artifacts/first_model_1.pt")
-
+        checkpoint_name=f"artifacts/{folder_name}/ckpt-{model.__class__.__name__}-val_acc-{avg_val_running_accuracy:.2f}-epoch-{epoch}.pth"
+        checkpoint={'epoch':epoch,
+                    'model_state_dict':model.state_dict(),
+                    'optimizer_state_dict':optimizer.state_dict(),
+                    'train_loss':avg_train_loss,
+                    'val_loss':avg_val_loss,
+                    'train_acc':avg_train_running_accuracy,
+                    'val_acc':avg_val_running_accuracy
+                  }
+        best_checkpoint_name=f"artifacts/{folder_name}/ckpt-{model.__class__.__name__}-best_val_acc-{best_val_accurcy:.2f}-epoch-{epoch}.pth"
+        
+        #save the model
+        torch.save(checkpoint,best_checkpoint_name)
+    
 
 
 
